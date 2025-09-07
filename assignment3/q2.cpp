575f3ca0058d13b98ecb66bcd48edc383a88bc15
VisheshJ15 <jain.vishesh1093@gmail.com>
2025-09-08 00:08:55 +0530

    Update q2.cpp

--- assignment3/q2.cpp
+++ assignment3/q2.cpp
@@ -1,57 +1,25 @@
 #include <iostream>
-#include <string>
-
+#include <stack>
 using namespace std;
 
-// Helper function to push a character onto the stack
-void push(char stack[], int &top, char item) {
-    top = top + 1;
-    stack[top] = item;
-}
-
-// Helper function to pop a character from the stack
-char pop(char stack[], int &top) {
-    char item = stack[top];
-    top = top - 1;
-    return item;
-}
-
-// Function to reverse a string using a stack
-void reverseString(string &s) {
-    int n = s.length();
-    if (n == 0) {
-        return; // Nothing to reverse
-    }
-
-    // Create a character stack using a dynamic array
-    char *stack = new char[n];
-    int top = -1;
-
-    // 1. Push all characters of the string onto the stack
-    for (int i = 0; i < n; i++) {
-        push(stack, top, s[i]);
-    }
-
-    // 2. Pop all characters from the stack back into the string
-    for (int i = 0; i < n; i++) {
-        s[i] = pop(stack, top);
-    }
-
-    // Free the allocated memory for the stack
-    delete[] stack;
-}
-
 int main() {
-    string input_str;
-    cout << "Enter the string to reverse: ";
-    // Use getline to correctly read strings with spaces
-    getline(cin, input_str);
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
 
-    cout << "Original string: " << input_str << endl;
+    stack<char> s;
 
-    reverseString(input_str);
+    for (int i = 0; i < str.length(); i++) {
+        s.push(str[i]);
+    }
 
-    cout << "Reversed string: " << input_str << endl;
+    string reversed = "";
+    while (!s.empty()) {
+        reversed += s.top();
+        s.pop();
+    }
+
+    cout << "Reversed string: " << reversed << endl;
 
     return 0;
 }
