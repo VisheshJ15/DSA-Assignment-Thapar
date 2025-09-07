9ac1d4c5beb9095ee95ab16d6f121deb2ca0ba29
VisheshJ15 <jain.vishesh1093@gmail.com>
2025-09-08 00:17:15 +0530

    Update q6a.cpp

--- assignment2/q6a.cpp
+++ assignment2/q6a.cpp
@@ -1,166 +1,97 @@
 #include <iostream>
 using namespace std;
 
-#define MAX 100
+#define MAX 20
 
-void readSparse(int mat[MAX][3]) {
-    int rows, cols, nonZero;
-    cout << "Enter rows, columns, and number of non-zero elements: ";
-    cin >> rows >> cols >> nonZero;
-
-    mat[0][0] = rows;
-    mat[0][1] = cols;
-    mat[0][2] = nonZero;
-
-    cout << "Enter row, column, value for each non-zero element:\n";
-    for (int i = 1; i <= nonZero; i++) {
-        cin >> mat[i][0] >> mat[i][1] >> mat[i][2];
+void read(int a[MAX][3]) {
+    int n;
+    cout << "Enter rows, cols and number of non-zero elements: ";
+    cin >> a[0][0] >> a[0][1] >> a[0][2];
+    n = a[0][2];
+    cout << "Enter row col value for each non-zero:\n";
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i][0] >> a[i][1] >> a[i][2];
     }
 }
 
-void printSparse(int mat[MAX][3]) {
-    int n = mat[0][2];
-    cout << "\nRow Col Value\n";
+void print(int a[MAX][3]) {
+    int n = a[0][2];
+    cout << "Row Col Val\n";
     for (int i = 0; i <= n; i++) {
-        cout << mat[i][0] << "   " << mat[i][1] << "   " << mat[i][2] << endl;
+        cout << a[i][0] << "   " << a[i][1] << "   " << a[i][2] << endl;
     }
 }
 
-void transpose(int mat[MAX][3], int trans[MAX][3]) {
-    int rows = mat[0][0], cols = mat[0][1], n = mat[0][2];
-    trans[0][0] = cols;
-    trans[0][1] = rows;
-    trans[0][2] = n;
-
+void transpose(int a[MAX][3], int t[MAX][3]) {
+    int rows = a[0][0], cols = a[0][1], n = a[0][2];
+    t[0][0] = cols; t[0][1] = rows; t[0][2] = n;
     int k = 1;
-    for (int col = 0; col < cols; col++) {
+    for (int c = 0; c < cols; c++) {
         for (int i = 1; i <= n; i++) {
-            if (mat[i][1] == col) {
-                trans[k][0] = mat[i][1];
-                trans[k][1] = mat[i][0];
-                trans[k][2] = mat[i][2];
+            if (a[i][1] == c) {
+                t[k][0] = a[i][1];
+                t[k][1] = a[i][0];
+                t[k][2] = a[i][2];
                 k++;
             }
         }
     }
 }
 
-void addSparse(int a[MAX][3], int b[MAX][3], int sum[MAX][3]) {
+void add(int a[MAX][3], int b[MAX][3], int s[MAX][3]) {
     if (a[0][0] != b[0][0] || a[0][1] != b[0][1]) {
-        cout << "Addition not possible. Sizes differ.\n";
-        return;
+        cout << "Addition not possible\n"; return;
     }
-
-    int i = 1, j = 1, k = 1;
-    sum[0][0] = a[0][0];
-    sum[0][1] = a[0][1];
-
-    while (i <= a[0][2] && j <= b[0][2]) {
-        if (a[i][0] < b[j][0] || (a[i][0] == b[j][0] && a[i][1] < b[j][1])) {
-            sum[k][0] = a[i][0];
-            sum[k][1] = a[i][1];
-            sum[k][2] = a[i][2];
-            i++; k++;
-        }
-        else if (b[j][0] < a[i][0] || (b[j][0] == a[i][0] && b[j][1] < a[i][1])) {
-            sum[k][0] = b[j][0];
-            sum[k][1] = b[j][1];
-            sum[k][2] = b[j][2];
-            j++; k++;
-        }
-        else {
-            sum[k][0] = a[i][0];
-            sum[k][1] = a[i][1];
-            sum[k][2] = a[i][2] + b[j][2];
+    int i=1,j=1,k=1;
+    s[0][0]=a[0][0]; s[0][1]=a[0][1];
+    while (i<=a[0][2] && j<=b[0][2]) {
+        if (a[i][0]==b[j][0] && a[i][1]==b[j][1]) {
+            s[k][0]=a[i][0]; s[k][1]=a[i][1]; s[k][2]=a[i][2]+b[j][2];
             i++; j++; k++;
+        } else if (a[i][0]<b[j][0] || (a[i][0]==b[j][0] && a[i][1]<b[j][1])) {
+            s[k][0]=a[i][0]; s[k][1]=a[i][1]; s[k][2]=a[i][2]; i++; k++;
+        } else {
+            s[k][0]=b[j][0]; s[k][1]=b[j][1]; s[k][2]=b[j][2]; j++; k++;
         }
     }
-
-    while (i <= a[0][2]) {
-        sum[k][0] = a[i][0];
-        sum[k][1] = a[i][1];
-        sum[k][2] = a[i][2];
-        i++; k++;
-    }
-
-    while (j <= b[0][2]) {
-        sum[k][0] = b[j][0];
-        sum[k][1] = b[j][1];
-        sum[k][2] = b[j][2];
-        j++; k++;
-    }
-
-    sum[0][2] = k - 1;
+    while (i<=a[0][2]) { s[k][0]=a[i][0]; s[k][1]=a[i][1]; s[k][2]=a[i][2]; i++; k++; }
+    while (j<=b[0][2]) { s[k][0]=b[j][0]; s[k][1]=b[j][1]; s[k][2]=b[j][2]; j++; k++; }
+    s[0][2]=k-1;
 }
 
-void multiplySparse(int a[MAX][3], int b[MAX][3], int prod[MAX][3]) {
-    if (a[0][1] != b[0][0]) {
-        cout << "Multiplication not possible. Sizes mismatch.\n";
-        return;
-    }
-
-    int transB[MAX][3];
-    transpose(b, transB);
-
-    prod[0][0] = a[0][0];
-    prod[0][1] = b[0][1];
-    int k = 1;
-
-    for (int i = 1; i <= a[0][2]; i++) {
-        for (int j = 1; j <= transB[0][2]; j++) {
-            if (a[i][1] == transB[j][1]) {
-                int row = a[i][0];
-                int col = transB[j][0];
-                int value = a[i][2] * transB[j][2];
-
-                bool found = false;
-                for (int x = 1; x < k; x++) {
-                    if (prod[x][0] == row && prod[x][1] == col) {
-                        prod[x][2] += value;
-                        found = true;
-                        break;
-                    }
+void multiply(int a[MAX][3], int b[MAX][3], int m[MAX][3]) {
+    if (a[0][1]!=b[0][0]) { cout<<"Multiplication not possible\n"; return; }
+    int t[MAX][3]; transpose(b,t);
+    m[0][0]=a[0][0]; m[0][1]=b[0][1]; int k=1;
+    for (int i=1;i<=a[0][2];i++) {
+        for (int j=1;j<=t[0][2];j++) {
+            if (a[i][1]==t[j][1]) {
+                int r=a[i][0], c=t[j][0], val=a[i][2]*t[j][2];
+                bool found=false;
+                for (int x=1;x<k;x++) {
+                    if (m[x][0]==r && m[x][1]==c) { m[x][2]+=val; found=true; break; }
                 }
-                if (!found) {
-                    prod[k][0] = row;
-                    prod[k][1] = col;
-                    prod[k][2] = value;
-                    k++;
-                }
+                if (!found) { m[k][0]=r; m[k][1]=c; m[k][2]=val; k++; }
             }
         }
     }
-
-    prod[0][2] = k - 1;
+    m[0][2]=k-1;
 }
 
 int main() {
-    int a[MAX][3], b[MAX][3], result[MAX][3];
+    int A[MAX][3], B[MAX][3], R[MAX][3];
+    cout<<"Enter first matrix:\n"; read(A);
+    cout<<"Triplet form:\n"; print(A);
 
-    cout << "Enter first sparse matrix:\n";
-    readSparse(a);
+    cout<<"\nTranspose:\n"; transpose(A,R); print(R);
 
-    cout << "First matrix in triplet form:";
-    printSparse(a);
+    cout<<"\nEnter second matrix:\n"; read(B);
+    cout<<"Triplet form:\n"; print(B);
 
-    cout << "\nTranspose of first matrix:";
-    transpose(a, result);
-    printSparse(result);
+    cout<<"\nAddition:\n"; add(A,B,R); print(R);
 
-    cout << "\nEnter second sparse matrix (same size for addition):\n";
-    readSparse(b);
-
-    cout << "Second matrix in triplet form:";
-    printSparse(b);
-
-    cout << "\nAddition of matrices:";
-    addSparse(a, b, result);
-    printSparse(result);
-
-    cout << "\nMultiplication of matrices:";
-    multiplySparse(a, b, result);
-    printSparse(result);
-
+    cout<<"\nMultiplication:\n"; multiply(A,B,R); print(R);
     return 0;
 }
+
 
