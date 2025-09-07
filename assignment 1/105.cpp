625339b239ed986225e413ba2fa0357222ad8bc0
VisheshJ15 <jain.vishesh1093@gmail.com>
2025-09-08 00:52:24 +0530

    Update 105.cpp

--- assignment 1/105.cpp
+++ assignment 1/105.cpp
@@ -1,39 +1,32 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int r, c;
-    cout << "Enter number of rows and columns: ";
-    cin >> r >> c;
-    int arr[r][c];
-    cout << "Enter elements of the matrix:\n";
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            cin >> arr[i][j];
+int main(){
+    int a[10][10],r,c;
+    cout << "Enter number of rows: ";
+    cin >> r;
+    cout << "ENter number of columns: ";
+    cin >> c;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin >> a[i][j];
         }
     }
-    cout << "\nMatrix:\n";
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            cout << arr[i][j] << " ";
+    for(int i=0;i<r;i++){
+        int sum =0;
+        for(int j=0;j<c;j++){
+            sum +=a[i][j];
         }
-        cout << endl;
+            cout << "Sum of row " << i+1 << "is " << sum << endl;
+        
     }
-    cout << "\nSum of each row:\n";
-    for (int i = 0; i < r; i++) {
-        int rowSum = 0;
-        for (int j = 0; j < c; j++) {
-            rowSum += arr[i][j];
+    for(int j=0;j<c;j++){
+        int sum = 0;
+        for(int i=0;i<r;i++){
+            sum +=a[i][j];
         }
-        cout << "Row " << i + 1 << ": " << rowSum << endl;
-    }
-    cout << "\nSum of each column:\n";
-    for (int j = 0; j < c; j++) {
-        int colSum = 0;
-        for (int i = 0; i < r; i++) {
-            colSum += arr[i][j];
-        }
-        cout << "Column " << j + 1 << ": " << colSum << endl;
+            cout << "Sum of every column " << j+1 << "is " << sum << endl;
+        
     }
     return 0;
 }
