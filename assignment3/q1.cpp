2071ad869804114e59efc5eb3ec145fd4b45b131
VisheshJ15 <jain.vishesh1093@gmail.com>
2025-09-08 00:07:45 +0530

    Update q1.cpp

--- assignment3/q1.cpp
+++ assignment3/q1.cpp
@@ -1,112 +1,102 @@
 #include <iostream>
 using namespace std;
 
-class Stack {
-    int arr[100];   
-    int top;        
-    int capacity;   
+#define MAX 5
 
-public:
+int stackArr[MAX];
+int top = -1;
 
-    Stack(int size) {
-        capacity = size;
-        top = -1;   
+bool isEmpty() {
+    return (top == -1);
+}
+
+bool isFull() {
+    return (top == MAX - 1);
+}
+
+void push(int value) {
+    if (isFull()) {
+        cout << "Stack Overflow! Cannot push " << value << endl;
+    } else {
+        top++;
+        stackArr[top] = value;
+        cout << value << " pushed into the stack." << endl;
     }
+}
 
-    bool isEmpty() {
-        return top == -1;
+void pop() {
+    if (isEmpty()) {
+        cout << "Stack Underflow! Cannot pop." << endl;
+    } else {
+        cout << stackArr[top] << " popped from the stack." << endl;
+        top--;
     }
+}
 
-    bool isFull() {
-        return top == capacity - 1;
+void peek() {
+    if (isEmpty()) {
+        cout << "Stack is empty, nothing to peek." << endl;
+    } else {
+        cout << "Top element is: " << stackArr[top] << endl;
     }
+}
 
-    void push(int value) {
-        if (isFull()) {
-            cout << "Stack Overflow! Cannot push " << value << endl;
-        } else {
-            arr[++top] = value;
-            cout << value << " pushed into stack.\n";
+void display() {
+    if (isEmpty()) {
+        cout << "Stack is empty." << endl;
+    } else {
+        cout << "Stack elements (top to bottom): ";
+        for (int i = top; i >= 0; i--) {
+            cout << stackArr[i] << " ";
         }
+        cout << endl;
     }
-
-    void pop() {
-        if (isEmpty()) {
-            cout << "Stack Underflow! Cannot pop.\n";
-        } else {
-            cout << arr[top--] << " popped from stack.\n";
-        }
-    }
-
-    void peek() {
-        if (isEmpty()) {
-            cout << "Stack is empty.\n";
-        } else {
-            cout << "Top element is: " << arr[top] << endl;
-        }
-    }
-
-    void display() {
-        if (isEmpty()) {
-            cout << "Stack is empty.\n";
-        } else {
-            cout << "Stack elements (top to bottom): ";
-            for (int i = top; i >= 0; i--) {
-                cout << arr[i] << " ";
-            }
-            cout << endl;
-        }
-    }
-};
+}
 
 int main() {
-    int size;
-    cout << "Enter size of stack: ";
-    cin >> size;
-
-    Stack s(size);
     int choice, value;
-
     do {
-        cout << "\n--- Stack Menu ---\n";
+        cout << "\n=== STACK MENU ===\n";
         cout << "1. Push\n";
         cout << "2. Pop\n";
         cout << "3. Peek\n";
         cout << "4. isEmpty\n";
         cout << "5. isFull\n";
         cout << "6. Display\n";
-        cout << "0. Exit\n";
-        cout << "Enter choice: ";
+        cout << "7. Exit\n";
+        cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
             case 1:
                 cout << "Enter value to push: ";
                 cin >> value;
-                s.push(value);
+                push(value);
                 break;
             case 2:
-                s.pop();
+                pop();
                 break;
             case 3:
-                s.peek();
+                peek();
                 break;
             case 4:
-                cout << (s.isEmpty() ? "Stack is empty.\n" : "Stack is not empty.\n");
+                if (isEmpty()) cout << "Yes, stack is empty." << endl;
+                else cout << "No, stack is not empty." << endl;
                 break;
             case 5:
-                cout << (s.isFull() ? "Stack is full.\n" : "Stack is not full.\n");
+                if (isFull()) cout << "Yes, stack is full." << endl;
+                else cout << "No, stack is not full." << endl;
                 break;
             case 6:
-                s.display();
+                display();
                 break;
-            case 0:
-                cout << "Exiting...\n";
+            case 7:
+                cout << "Exiting program. Goodbye!" << endl;
                 break;
             default:
-                cout << "Invalid choice!\n";
+                cout << "Invalid choice! Try again." << endl;
         }
-    } while (choice != 0);
+    } while (choice != 7);
 
     return 0;
 }
